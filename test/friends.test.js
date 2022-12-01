30ce346a115440c17f2a1bcf54b6a19b9805dcad
Doga Oztuzun <unquale@gmail.com>
2022-12-01 21:10:08 +0300

    test for adding friend

--- test/friends.test.js
+++ test/friends.test.js
@@ -7,7 +7,18 @@ contract('Friends', (accounts) => {
         const friendsInstance = await Friends.deployed(usersInstance.address);
 
         const me = await friendsInstance.showMe();
-        
+
         assert.include(me, "s0xAdmin", "admin");
+    });
+
+    it('should add friend', async () => {
+        const usersInstance = await Users.deployed();
+        const friendsInstance = await Friends.deployed(usersInstance.address);
+
+        await friendsInstance.follow(accounts[0], accounts[1]);
+
+        const followingCount = await friendsInstance.doDegenzCount(accounts[1]);
+
+        assert.equal(followingCount, 1, "following count");
     });
 });
